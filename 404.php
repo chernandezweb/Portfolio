46ae5d29e99bdc7ddcff330b7fc259bd0f829ec6
Carlos Hernandez <chernandezweb@gmail.com>
2018-07-13 11:29:24 -0400

    Changes on images

---
+++ 404.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>404</title>
+</head>
+<body>
+<h1>
+    Tu t'es perdu ! retrouve ta maman !
+</h1>
+</body>
+</html>
